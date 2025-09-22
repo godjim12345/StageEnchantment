e6923b1d7ed7bd67127d894fd74bd387baa7415d
gam0zing <godjim12345@hotmail.com>
2025-09-22 17:23:08 +0800

    修改注释

--- src/main/java/com/gam0zing/stage_enchantment/command_pattern/EnchCommand.java
+++ src/main/java/com/gam0zing/stage_enchantment/command_pattern/EnchCommand.java
@@ -7,10 +7,10 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import java.util.Map;
 
 /// 使用这个Command的要求：
-/// 需要把生成的Command对象放在一个Map里作为Value
-/// Map的Key可以是任何东西，数字、字符串、UUID，只要方便使用指令的时候输入查找就可以
-/// 和最大等级那张Map一样，也是一个存档对应一个，所以需要生成Json文件在SeverConfig里，名字就叫stage_enchantment-commands.json
-/// json的格式不重要，完全存储需要的值，便于读写即可
+/// 需要把生成的Command对象放在一个Map里作为Value。
+/// Map的Key可以是任何东西，数字、字符串、UUID，只要方便使用指令的时候输入查找就可以。
+/// 和最大等级那张Map一样，也是一个存档对应一个，所以需要生成Json文件在SeverConfig里，名字就叫stage_enchantment-commands.json。
+/// json的格式不重要，完全存储需要的值，便于读写即可。
 public class EnchCommand implements ICommand {
 
     private final Map<Enchantment, Integer> effects;
