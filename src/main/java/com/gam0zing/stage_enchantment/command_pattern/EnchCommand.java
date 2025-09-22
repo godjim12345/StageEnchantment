354ab4678a097170109c038b11868a3875599801
gam0zing <godjim12345@hotmail.com>
2025-09-22 17:19:31 +0800

    添加命令模式模块相关注释和添加、删除Command效果的方法

--- src/main/java/com/gam0zing/stage_enchantment/command_pattern/EnchCommand.java
+++ src/main/java/com/gam0zing/stage_enchantment/command_pattern/EnchCommand.java
@@ -6,6 +6,11 @@ import net.minecraft.world.item.enchantment.Enchantment;
 
 import java.util.Map;
 
+// 使用这个Command的要求：
+// 需要把生成的Command对象放在一个Map里作为Value
+// Map的Key可以是任何东西，数字、字符串、UUID，只要方便使用指令的时候输入查找就可以
+// 和最大等级那张Map一样，也是一个存档对应一个，所以需要生成Json文件在SeverConfig里，名字就叫stage_enchantment-commands.json
+// json的格式不重要，完全存储需要的值，便于读写即可
 public class EnchCommand implements ICommand {
 
     private final Map<Enchantment, Integer> effects;
@@ -16,6 +21,8 @@ public class EnchCommand implements ICommand {
         this.executed = false;
     }
 
+    //执行方法
+    //加在指令里
     @Override
     public boolean execute() {
 
@@ -28,6 +35,8 @@ public class EnchCommand implements ICommand {
         return true;
     }
 
+    //撤销方法
+    //加在指令里
     @Override
     public boolean unexecute() {
 
@@ -38,6 +47,32 @@ public class EnchCommand implements ICommand {
         executed = false;
 
         return true;
+    }
+
+    //添加或修改对应附魔的效果并重新应用
+    //加在指令里
+    public void addEffect(Enchantment ench, Integer value) {
+
+        boolean flag = this.executed;
+
+        if (flag) unexecute();
+
+        effects.put(ench, value);
+
+        if (flag) execute();
+    }
+
+    //删除对应附魔的效果并重新应用
+    //加在指令里
+    public void removeEffect(Enchantment ench) {
+
+        boolean flag = this.executed;
+
+        if (flag) unexecute();
+
+        effects.remove(ench);
+
+        if (flag) execute();
     }
 
     private void doEffect(Enchantment ench, Integer value) {
